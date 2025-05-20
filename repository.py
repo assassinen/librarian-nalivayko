2900eeaf1a923fc1862dc4c8f48dab913513d440
inovikov <inovikov@astralinux.ru>
2025-05-20 18:30:40 +0300

    ver.01: косметика

--- repository.py
+++ repository.py
@@ -5,19 +5,20 @@ from schemas import STaskAdd, STask
 class TaskRepository:
     @classmethod
     async def add_task(cls, task: STaskAdd) -> int:
-         async with new_session() as session:
-              data = task.model_dump()
-              new_task = TaskOrm(**data)
-              session.add(new_task)
-              await session.flush()
-              await session.commit()
-              return new_task.id
+        async with new_session() as session:
+            data = task.model_dump()
+            new_task = TaskOrm(**data)
+            session.add(new_task)
+            await session.flush()
+            await session.commit()
+            return new_task.id
+          
 
     @classmethod
     async def get_tasks(cls) -> list[STask]:
-         async with new_session() as session:
-              query = select(TaskOrm)
-              result = await session.execute(query)
-              task_models = result.scalars().all()
-              tasks = [STask.model_validate(task_model) for task_model in task_models]
-              return tasks
+        async with new_session() as session:
+            query = select(TaskOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            tasks = [STask.model_validate(task_model) for task_model in task_models]
+            return tasks
