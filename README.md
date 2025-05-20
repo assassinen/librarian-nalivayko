9dede7ef90b2d7cc7ddc5299407cfc5cc7f0ec93
inovikov <inovikov@astralinux.ru>
2025-05-20 18:00:44 +0300

    ver.01: быстрый прототип

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # librarian-nalivayko
 https://docs.google.com/document/d/1ej6Qdhf65VP6d8rPCti2wdiD680p91UKu6GQS7i-IKs/edit?tab=t.0
+
+# первая итерация по инструкции:
+https://selectel.ru/blog/tutorials/how-to-develop-fastapi-application/
---
+++ main.py
@@ -0,0 +1,18 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    print("База delete_tables")
+    yield
+    await delete_tables()
+    print("База очищена")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
